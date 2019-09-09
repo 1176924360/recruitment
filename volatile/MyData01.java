da6e6ded27c9c69ce16def6d5284466992b15443
cdvsdsa <37651006+cdvsdsa@users.noreply.github.com>
2019-09-09 09:50:27 +0800

    Update MyData01.java

--- volatile/MyData01.java
+++ volatile/MyData01.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 class MyData01{
     //当number不被修饰为volatile，其对内存不可见，当有两个线程时(一个main线程，一个AAA线程)，AAA线程将number
     //变成60后，main线程无法得知此信息
-    int number = 0;
+    number = 0;
     public void addTo60(){
         number = 60;
     }
@@ -36,15 +36,5 @@ public class VolatileDemo {
         }
 
         System.out.println(Thread.currentThread().getName() + "\t mission is over");
-
-//        for (int i = 0; i < 20; i++) {
-//            new Thread(() -> {
-//                for (int j = 0; j < 10; j++) {
-//                    myData.addPlus();
-//                }
-//            }, String.valueOf(i)).start();
-//            System.out.println(Thread.currentThread().getName() + "\t finally number value:" + myData.number);
-//        }
-//        System.out.println(Thread.currentThread().getName() + "\t finally number value:" + myData.number);
     }
 }
