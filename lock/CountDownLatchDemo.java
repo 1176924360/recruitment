0ec1bdb09b4a12d0339dea0bb4d7ad249ef5a7c9
cdvsdsa <37651006+cdvsdsa@users.noreply.github.com>
2019-09-09 15:30:51 +0800

    Update CountDownLatchDemo.java

--- lock/CountDownLatchDemo.java
+++ lock/CountDownLatchDemo.java
@@ -5,7 +5,10 @@ import java.util.concurrent.CountDownLatch;
 /**
  * @author Ethan
  * @date 2019/9/9 15:08
- * 只有当其他线程完成时，本线程才可以开始
+  * 只有当其他线程完成时，本线程才可以开始
+ * 让一些线程阻塞直到另一些线程完成一系列操作后才被唤醒
+ * countDownLatch有两个方法，一个await()，调用的线程会阻塞，其他线程调用countDown()将计数器减一。
+ * 当计数器变成0时，被await()阻塞的线程会被唤起
  */
 public class CountDownLatchDemo {
     public static void main(String[] args) throws InterruptedException {
